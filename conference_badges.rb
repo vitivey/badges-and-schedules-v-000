d5d99bcf27a819ddec1b24cff4339fb0f9949e13
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-08 00:39:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,10 +16,6 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  batch_badge_creator(array).each do |badge|
-    puts "#{badge}"
-  end
-  assign_rooms(array).each do |room|
-    puts "#{room}"
-  end
+  puts batch_badge_creator(array)
+  puts assign_rooms(array)
 end
